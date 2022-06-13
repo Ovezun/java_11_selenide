bfd24fcc087e70e4fa8e4b46943718f9e203ecdb
vezun <zhadina2@ya.ru>
2022-06-13 20:25:58 +0700

    add search Wiki Selenide test

---
+++ src/test/java/SelenideWikiTest.java
@@ -0,0 +1,28 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import org.junit.jupiter.api.Test;
+
+
+public class SelenideWikiTest {
+
+    @BeforeAll
+    static void beforeAll(){
+        Configuration.browserSize="1920x1080";
+        Configuration.baseUrl = "https://github.com";
+    }
+    @Test
+    void selenideTest() {
+        open("/selenide/selenide");
+        $("#wiki-tab").click();
+        $(byText("Soft assertions")).click();
+        $ ("#user-content-3-using-junit5-extend-test-class").parent().shouldHave(text("Unit5 extend test"));
+
+    }
+
+
+
+}
